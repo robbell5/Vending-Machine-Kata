7b001b7a99599c863098ad2c00eb7f04931cdfc8
Rob <rob.bell.86@gmail.com>
2016-01-11 18:24:41 -0500

    Initial Commit

---
+++ Vending Machine Kata Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vending_Machine_Kata_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
