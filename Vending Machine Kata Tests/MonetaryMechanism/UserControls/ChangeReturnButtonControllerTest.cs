6ee1314618b6f6caaac90a1a3ae6f6d61dca3ad1
Rob <rob.bell.86@gmail.com>
2016-01-12 17:23:02 -0500

    ChangeReturnButtonController now takes a CoinPurse and a CoinReturn

--- Vending Machine Kata Tests/MonetaryMechanism/UserControls/ChangeReturnButtonControllerTest.cs
+++ Vending Machine Kata Tests/MonetaryMechanism/UserControls/ChangeReturnButtonControllerTest.cs
@@ -16,9 +16,15 @@ namespace Vending_Machine_Kata_Tests.MonetaryMechanism.UserControls
         public void TestProperties()
         {
             Button expectedChangeReturnButton = new Button();
-            ChangeReturnButtonController changeReturnButtonController = new ChangeReturnButtonController(expectedChangeReturnButton);
+            MockCoinPurse expectedCoinPurse = new MockCoinPurse();
+            MockCoinReturn expectedCoinReturn = new MockCoinReturn();
+
+            ChangeReturnButtonController changeReturnButtonController =
+                new ChangeReturnButtonController(expectedChangeReturnButton, expectedCoinPurse, expectedCoinReturn);
 
             Assert.AreEqual(expectedChangeReturnButton, changeReturnButtonController.Button);
+            Assert.AreEqual(expectedCoinPurse, changeReturnButtonController.CoinPurse);
+            Assert.AreEqual(expectedCoinReturn, changeReturnButtonController.CoinReturn);
         }
     }
 }
--- Vending Machine Kata/MonetaryMechanism/UserControls/ChangeReturnButtonController.cs
+++ Vending Machine Kata/MonetaryMechanism/UserControls/ChangeReturnButtonController.cs
@@ -5,10 +5,14 @@ namespace Vending_Machine_Kata.MonetaryMechanism.UserControls
     public class ChangeReturnButtonController
     {
         public Button Button { get; set; }
+        public ICoinPurse CoinPurse { get; set; }
+        public ICoinReturn CoinReturn { get; set; }
 
-        public ChangeReturnButtonController(Button changeReturnButton)
+        public ChangeReturnButtonController(Button changeReturnButton, ICoinPurse coinPurse, ICoinReturn coinReturn)
         {
             Button = changeReturnButton;
+            CoinPurse = coinPurse;
+            CoinReturn = coinReturn;
         }
     }
 }
