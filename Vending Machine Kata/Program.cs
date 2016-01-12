781f62e78f97ceca593e3e7a3402c3bcf2e0d7ef
Rob <rob.bell.86@gmail.com>
2016-01-12 17:00:57 -0500

    Removed default added Form

--- Vending Machine Kata/Program.cs
+++ Vending Machine Kata/Program.cs
@@ -16,7 +16,6 @@ namespace Vending_Machine_Kata
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
