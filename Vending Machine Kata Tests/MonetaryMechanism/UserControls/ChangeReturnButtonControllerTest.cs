4ee497c357b653f1a43d2ea238eb698eb4f0620b
Rob <rob.bell.86@gmail.com>
2016-01-12 17:17:45 -0500

    Created ChangeReturnButtonController

---
+++ Vending Machine Kata Tests/MonetaryMechanism/UserControls/ChangeReturnButtonControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Vending_Machine_Kata.MonetaryMechanism.UserControls;
+
+namespace Vending_Machine_Kata_Tests.MonetaryMechanism.UserControls
+{
+    [TestFixture]
+    public class ChangeReturnButtonControllerTest
+    {
+        [Test]
+        public void TestProperties()
+        {
+            Button expectedChangeReturnButton = new Button();
+            ChangeReturnButtonController changeReturnButtonController = new ChangeReturnButtonController(expectedChangeReturnButton);
+
+            Assert.AreEqual(expectedChangeReturnButton, changeReturnButtonController.Button);
+        }
+    }
+}
---
+++ Vending Machine Kata/MonetaryMechanism/UserControls/ChangeReturnButtonController.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Vending_Machine_Kata.MonetaryMechanism.UserControls
+{
+    public class ChangeReturnButtonController
+    {
+        public Button Button { get; set; }
+
+        public ChangeReturnButtonController(Button changeReturnButton)
+        {
+            Button = changeReturnButton;
+        }
+    }
+}
