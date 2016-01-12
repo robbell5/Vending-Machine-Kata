ad25451f649f2ebc03845045b4de3ad704350375
Rob <rob.bell.86@gmail.com>
2016-01-12 14:50:03 -0500

    Created new CoinSize Enum

---
+++ Vending Machine Kata Tests/MonetaryMechanism/Coin/CoinSizeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.MonetaryMechanism.Coin;
+
+namespace Vending_Machine_Kata_Tests.MonetaryMechanism.Coin
+{
+    [TestFixture]
+    public class CoinSizeTest
+    {
+        [TestCase(CoinSize.TINY, 1)]
+        [TestCase(CoinSize.SMALL, 2)]
+        [TestCase(CoinSize.MEDIUM, 3)]
+        [TestCase(CoinSize.LARGE, 4)]
+        public void TestEnumValues(CoinSize coinSize, int expectedValue)
+        {
+            Assert.AreEqual(expectedValue, coinSize.GetHashCode());
+        }
+    }
+}
---
+++ Vending Machine Kata/MonetaryMechanism/Coin/CoinSize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vending_Machine_Kata.MonetaryMechanism.Coin
+{
+    public enum CoinSize
+    {
+        TINY = 1,
+        SMALL = 2,
+        MEDIUM = 3,
+        LARGE = 4
+    }
+}
