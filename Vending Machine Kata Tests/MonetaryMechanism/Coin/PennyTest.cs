caf36a4180f05ecc5fe7963762a1d509946ed8a2
Rob <rob.bell.86@gmail.com>
2016-01-12 14:34:48 -0500

    Created the Penny

---
+++ Vending Machine Kata Tests/MonetaryMechanism/Coin/PennyTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.MonetaryMechanism.Coin;
+
+namespace Vending_Machine_Kata_Tests.MonetaryMechanism.Coin
+{
+    [TestFixture]
+    public class PennyTest
+    {
+        [Test]
+        public void TestImplementsInterface()
+        {
+            Assert.IsInstanceOf(typeof(ICoin), new Penny());
+        }
+
+        [Test]
+        public void TestValue()
+        {
+            const decimal expectedNickleValue = 0.01m;
+            Assert.AreEqual(expectedNickleValue, new Penny().Value);
+        }
+    }
+}
---
+++ Vending Machine Kata/MonetaryMechanism/Coin/Penny.cs
@@ -0,0 +1,7 @@
+namespace Vending_Machine_Kata.MonetaryMechanism.Coin
+{
+    public class Penny : ICoin
+    {
+        public decimal Value => 0.01m;
+    }
+}
