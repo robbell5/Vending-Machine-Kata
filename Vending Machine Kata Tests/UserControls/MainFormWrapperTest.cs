3b0d7d084ed2acf7a26e3f4cca92ae62096a2a0b
Rob <rob.bell.86@gmail.com>
2016-01-12 22:10:52 -0500

    Created MainFormWrapper

---
+++ Vending Machine Kata Tests/UserControls/MainFormWrapperTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Vending_Machine_Kata.UserControls;
+
+namespace Vending_Machine_Kata_Tests.UserControls
+{
+    [TestFixture]
+    public class MainFormWrapperTest
+    {
+        [Test]
+        public void TestProperties()
+        {
+            Form expectedForm = new Form();
+            MainFormWrapper mainFormWrapper = new MainFormWrapper(expectedForm);
+
+            Assert.AreSame(expectedForm, mainFormWrapper.MainForm);
+        }
+    }
+}
---
+++ Vending Machine Kata/UserControls/MainFormWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace Vending_Machine_Kata.UserControls
+{
+    public class MainFormWrapper
+    {
+        public Form MainForm { get; private set; }
+
+        public MainFormWrapper(Form form)
+        {
+            MainForm = form;
+        }
+    }
+}
