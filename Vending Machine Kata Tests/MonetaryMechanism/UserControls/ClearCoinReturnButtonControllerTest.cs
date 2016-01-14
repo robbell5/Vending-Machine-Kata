45ad8b3f0e0f6840bebb19b91ddaf77d7ef55753
Rob <rob.bell.86@gmail.com>
2016-01-13 19:04:45 -0500

    Created ClearCoinReturnButtonController

---
+++ Vending Machine Kata Tests/MonetaryMechanism/UserControls/ClearCoinReturnButtonControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.MonetaryMechanism.UserControls;
+
+namespace Vending_Machine_Kata_Tests.MonetaryMechanism.UserControls
+{
+    [TestFixture]
+    public class ClearCoinReturnButtonControllerTest
+    {
+        [Test]
+        public void TestProperties()
+        {
+            MockCoinReturn expectedCoinReturn = new MockCoinReturn();
+            ClearCoinReturnButtonController clearCoinReturnButtonController = new ClearCoinReturnButtonController(expectedCoinReturn);
+
+            Assert.AreSame(expectedCoinReturn, clearCoinReturnButtonController.CoinReturn);
+        }
+    }
+}
---
+++ Vending Machine Kata/MonetaryMechanism/UserControls/ClearCoinReturnButtonController.cs
@@ -0,0 +1,12 @@
+﻿namespace Vending_Machine_Kata.MonetaryMechanism.UserControls
+{
+    public class ClearCoinReturnButtonController
+    {
+        public ICoinReturn CoinReturn { get; }
+
+        public ClearCoinReturnButtonController(ICoinReturn coinReturn)
+        {
+            CoinReturn = coinReturn;
+        }
+    }
+}
