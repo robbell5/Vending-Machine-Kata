96db04105b60c1929b42c94bde4d0d40484e3416
Rob <rob.bell.86@gmail.com>
2016-01-17 12:38:57 -0500

    Created DialogService which is a simple wrapper around MessageBox.Show

---
+++ Vending Machine Kata Tests/Dialog/DialogServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.Dialog;
+
+namespace Vending_Machine_Kata_Tests.Dialog
+{
+    [TestFixture]
+    public class DialogServiceTest
+    {
+        [Test]
+        public void TestImplementsInterface()
+        {
+            Assert.IsInstanceOf(typeof(IDialogService), new DialogService());
+        }
+    }
+}
---
+++ Vending Machine Kata/Dialog/DialogService.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Vending_Machine_Kata.Dialog
+{
+    public class DialogService : IDialogService
+    {
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
