a3e04dbca32cdc8991dce7bbf7250da245e435a5
Rob <rob.bell.86@gmail.com>
2016-01-17 13:50:37 -0500

    Created Candy

---
+++ Vending Machine Kata Tests/Product/CandyTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.Product;
+
+namespace Vending_Machine_Kata_Tests.Product
+{
+    [TestFixture]
+    public class CandyTest
+    {
+        [Test]
+        public void TestImplementsInterface()
+        {
+            Assert.IsInstanceOf(typeof (IProduct), new Candy());
+        }
+    }
+}
---
+++ Vending Machine Kata/Product/Candy.cs
@@ -0,0 +1,8 @@
+﻿namespace Vending_Machine_Kata.Product
+{
+    public class Candy : IProduct
+    {
+        public string DisplayName { get; }
+        public decimal Value { get; }
+    }
+}
