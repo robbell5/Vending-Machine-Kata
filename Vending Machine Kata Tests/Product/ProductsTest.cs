52ad3305728b1c2328b8606a288baa89f6b41ff5
Rob <rob.bell.86@gmail.com>
2016-01-17 13:58:55 -0500

    Test Products has all three product types: Chips, Candy, Cola

---
+++ Vending Machine Kata Tests/Product/ProductsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vending_Machine_Kata.Product;
+
+namespace Vending_Machine_Kata_Tests.Product
+{
+    [TestFixture]
+    public class ProductsTest
+    {
+        [Test]
+        public void TestProductsTypes()
+        {
+            Assert.IsInstanceOf(typeof(Cola), Products.Cola);
+            Assert.IsInstanceOf(typeof(Chips), Products.Chips);
+            Assert.IsInstanceOf(typeof(Candy), Products.Candy);
+        }
+    }
+}
---
+++ Vending Machine Kata/Product/Products.cs
@@ -0,0 +1,9 @@
+﻿namespace Vending_Machine_Kata.Product
+{
+    public class Products
+    {
+        public static Cola Cola => new Cola();
+        public static object Chips => new Chips();
+        public static object Candy => new Candy();
+    }
+}
