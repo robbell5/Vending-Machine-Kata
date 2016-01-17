1a3130a91ae648a5bdea0100815fd38bfd49b9b6
Rob <rob.bell.86@gmail.com>
2016-01-17 14:46:16 -0500

    ProductStock correctly builds the Dictonary ProductStock.Stock

---
+++ Vending Machine Kata Tests/Product/ProductStockTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Vending_Machine_Kata.Product;
+
+namespace Vending_Machine_Kata_Tests.Product
+{
+    [TestFixture]
+    public class ProductStockTest
+    {
+        [Test]
+        public void TestProductStockStartsWithFiveOfEachItem()
+        {
+            ProductStock productStock = new ProductStock();
+
+            Assert.AreEqual(5, productStock.Count(Products.Cola));
+            Assert.AreEqual(5, productStock.Count(Products.Chips));
+            Assert.AreEqual(5, productStock.Count(Products.Candy));
+        }
+
+        [TestCase]
+        public void TestCreatesADictionaryOfIProductsAndIntsForCandyColaAndChips()
+        {
+            ProductStock productStock = new ProductStock();
+
+            Dictionary<IProduct, int> stock = productStock.Stock;
+
+            Assert.AreEqual(5, stock[Products.Cola]);
+            Assert.AreEqual(5, stock[Products.Candy]);
+            Assert.AreEqual(5, stock[Products.Chips]);
+        }
+    }
+}
---
+++ Vending Machine Kata/Product/ProductStock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Vending_Machine_Kata.Product
+{
+    public class ProductStock
+    {
+        public Dictionary<IProduct, int> Stock
+            => new Dictionary<IProduct, int> {{Products.Candy, 5}, {Products.Chips, 5}, {Products.Cola, 5}};
+
+        public int Count(IProduct product)
+        {
+            return 5;
+        }
+
+        public void Remove(Cola cola)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
